6c7cb3c9c7246df16f628e7d75a2a37d58f3d2e1
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-07-23 15:42:24 +0200

    Changed mana formula.
    Also moved the formula for mana points into universe model.
    Also display the points that would be gained on the rebirth button.

--- app/controllers/universe/templates.js
+++ app/controllers/universe/templates.js
@@ -4,13 +4,13 @@ export default Controller.extend({
   actions: {
     async rebirth(event) {
       event.preventDefault();
+      let manaPoints = this.model.nextManaPoints
       let oldEmpire = await this.model.get('mainEmpire')
-      let oldPop = oldEmpire.population
       oldEmpire.destroyRecord()
       let newEmpire = await this.store.createRecord('empire');
       await newEmpire.save();
       this.model.set('mainEmpire', newEmpire)
-      this.model.set('mana', this.model.get('mana') + oldPop)
+      this.model.set('mana', this.model.get('mana') + manaPoints)
       await this.model.save()
     },
   },
--- app/models/universe.js
+++ app/models/universe.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 const { Model, attr, belongsTo } = DS;
 
 export default Model.extend({
@@ -8,4 +9,16 @@ export default Model.extend({
   culture: attr('number', { defaultValue: 0}),
   money: attr('number', { defaultValue: 0}),
   science: attr('number', { defaultValue: 0}),
+
+  nextManaPoints: computed('mainEmpire.population', 'mainEmpire.turn', function(){
+    let pop = this.mainEmpire.get('population')
+    let turn = this.mainEmpire.get('turn')
+    if ( pop > 1 && turn > 20) {
+      return Math.max(0, Math.floor(Math.sqrt(
+        (pop-1)*(turn/10-1)
+      )))
+    } else {
+      return 0
+    }
+  }),
 });
