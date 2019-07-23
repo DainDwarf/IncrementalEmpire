933191fd53cd1b74d06f8811293aa608aeaebee2
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-07-23 18:51:58 +0200

    Not helping save reset.

--- app/controllers/universe/settings.js
+++ app/controllers/universe/settings.js
@@ -6,10 +6,10 @@ export default Controller.extend({
       event.preventDefault();
       let destroy = window.confirm('Are you sure? This will lose all your game progress and cannot be recovered in any way.')
       if (destroy) {
-        window.localStorage.clear();
+        // TODO: I don't know why this does not work. :(
+        await window.localStorage.clear();
+        await this.game.load()
         this.transitionToRoute('index')
-        // TODO: Find a way to refresh and get a new game state
-        // Possibly, by isolating the 'new game' into a function, and reuse it here.
       }
     },
   },
