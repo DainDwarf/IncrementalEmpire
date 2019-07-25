dddf9a1f1507927f4d05ccea8c85151aa062e60f
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-07-25 19:09:46 +0200

    Do not display bought upgrades.
    Maybe later add a tab/an option to display bought upgrades?

--- app/controllers/upgrades.js
+++ app/controllers/upgrades.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
+import { filterBy } from '@ember/object/computed';
 
 export default Controller.extend({
+  displayedUpgrades: filterBy('model', 'isActive', false),
+
   actions: {
     async buy(upgrade) {
       await this.game.buyUpgrade(upgrade)
