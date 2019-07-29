ad4a506bfd7cddf1ba6d786a7553eef56c11a749
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-07-29 17:44:04 +0200

    Renamed "Eden is working" achievement.

--- app/services/game-template.js
+++ app/services/game-template.js
@@ -26,7 +26,7 @@ export default Service.extend({
     ]
     this.achievements = []
     var ach
-    ach = await this.store.createRecord('achievement', {name: 'Eden is Working!', templatePoint: 1, description: 'Time to create Eve'})
+    ach = await this.store.createRecord('achievement', {name: 'Reach turn 10', templatePoint: 1, description: 'You can now assign template points to restart with more population'})
     ach.reopen({condition: gte('game.empire.turn', 10)})
     this.achievements.push(ach)
     ach = await this.store.createRecord('achievement', {name: 'Have 5 population', templatePoint: 1, description: 'Template Point gives 4x more population'})
