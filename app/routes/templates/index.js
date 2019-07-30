41b9c01fcfbd51b7a649662d6f740f03235d720c
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-07-30 15:18:34 +0200

    Adding save of template tab navigation.

--- app/routes/templates/index.js
+++ app/routes/templates/index.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  redirect() {
+    let t = this.controllerFor('templates').tabRouteObj
+    if (t != undefined) {
+      this.transitionTo('templates.template', t)
+    }
+  },
 });
