dc0e5f912308cea4254a774e3975cef8f400544c
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-07-30 17:40:11 +0200

    Corrected linter error.

--- app/components/display-value.js
+++ app/components/display-value.js
@@ -2,19 +2,22 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  typeToIcon: {
-    mana: 'dot-circle',
-    culture: 'theater-masks',
-    money: 'coins',
-    science: 'flask',
-    population: 'users',
-    happiness: 'smile',
-    magic: 'magic',
+  typeToIcon (type) {
+    let trans = {
+      mana: 'dot-circle',
+      culture: 'theater-masks',
+      money: 'coins',
+      science: 'flask',
+      population: 'users',
+      happiness: 'smile',
+      magic: 'magic',
+    }
+    return trans[type]
   },
 
   type: '',
   icon: computed('type', function() {
-    let ret = this.typeToIcon[this.type]
+    let ret = this.typeToIcon(this.type)
     return ret
   }),
 });
