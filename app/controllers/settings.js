8c39625be695bdcbb64f21558690e5dab3bfd166
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-07-30 09:31:45 +0200

    Adding basic export/import.

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -1,7 +1,18 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  saveData: '',
+
   actions: {
+    async importEverything(event) {
+      this.store.importData(this.saveData);
+      await this.game.load()
+    },
+    exportEverything() {
+      this.store.exportData
+        (['universes', 'empires', 'upgrades', 'achievements', 'templates'])
+        .then(data => this.set('saveData', data))
+    },
     async destroyEverything(event) {
       event.preventDefault();
       let destroy = window.confirm('Are you sure? This will lose all your game progress and cannot be recovered in any way.')
