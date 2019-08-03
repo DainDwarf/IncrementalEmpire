04eef3bdcfcf20c9b56c23894bb681261010ef21
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-03 16:32:54 +0200

    Defer template creation on modal.

--- app/controllers/templates.js
+++ app/controllers/templates.js
@@ -4,13 +4,18 @@ import { lt } from '@ember/object/computed';
 export default Controller.extend({
   tabRouteObj: undefined, //Instead of remembering the route to open, remember the template object
   canAddTemplate: lt('model.length', 3),
+  newTemplateModal: false,
 
   actions: {
-    async newTemplate(event) {
-      event.preventDefault()
+    setTemplateModal(val) {
+      this.set('newTemplateModal', val)
+    },
+
+    async newTemplate(type) {
       let t = await this.store.createRecord('template')
       this.game.templates.pushObject(t)
       await t.save()
+      this.set('newTemplateModal', false)
       this.transitionToRoute('templates.template', t.id)
     },
 
