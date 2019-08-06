43a9b256fdcaf007a09f8a9740de11c841828431
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-06 23:15:11 +0200

    Reviewed the achievement for second template (100 mana instead of 1000)

--- app/services/game-template.js
+++ app/services/game-template.js
@@ -69,9 +69,9 @@ export default Service.extend({
       conditionFactory: (a) => defineProperty(a, 'condition', gte('game.empire.population', 1000))
     })
     this.achievements.push(ach)
-    ach = await this.store.createRecord('achievement', {isHidden: false, name: 'Reach 1000 mana', templatePoint: 0, description: 'You can have one more empire template'})
+    ach = await this.store.createRecord('achievement', {isHidden: false, name: 'Reach 100 mana', templatePoint: 0, description: 'You can have one more empire template'})
     ach.reopen({
-      conditionFactory: (a) => defineProperty(a, 'condition', gte('game.universe.mana', 1000))
+      conditionFactory: (a) => defineProperty(a, 'condition', gte('game.universe.mana', 100))
     })
     this.achievements.push(ach)
     ach = await this.store.createRecord('achievement', {isHidden: true, name: 'Lose an empire', templatePoint: 1, description: 'You let all the population die, you monster!'})
