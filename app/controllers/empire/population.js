a6a979935c52503eabbc5e2d7b9d6a28f98f7a32
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-11 17:35:55 +0200

    Added construction of houses.

--- app/controllers/empire/population.js
+++ app/controllers/empire/population.js
@@ -17,6 +17,11 @@ export default Controller.extend({
   workerBreederAvailable: computed('model.type', 'game.upgrades.@each.isActive',function() {
     return this.game.getUpgrade('Birth').isActive && (this.model.type == "economical" || this.game.getUpgrade('Universal Worker').isActive)
   }),
+  maxPendingPopStorage: computed('model.{workerPopStorage,availableWorkers,material}', function () {
+    return Math.min(this.model.workerPopStorage+this.model.availableWorkers,
+      Math.floor(this.model.material/100)
+    )
+  }),
 
   actions: {
     async genPopulation(event) {
@@ -27,6 +32,12 @@ export default Controller.extend({
     },
     async changeBreeder(qty) {
       this.model.set('workerBreeder', qty)
+      await this.model.save()
+    },
+    async changePendingPopStorage(qty) {
+      let change = qty - this.model.pendingPopStorage
+      this.model.set('pendingPopStorage', qty)
+      this.model.set('material', this.model.material-100*change)
       await this.model.save()
     },
   },
--- app/models/empire.js
+++ app/models/empire.js
@@ -1,23 +1,34 @@
 import DS from 'ember-data';
 const { Model, attr } = DS;
 import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 export default Model.extend({
   name: attr('string', {defaultValue: 'Empire'}),
   type: attr('string', {defaultValue: 'religious'}),
   turn: attr('number', {defaultValue: 0}),
-  population: attr('number', { defaultValue: 1}),
   dead: attr('boolean', { defaultValue: false}),
+
+  population: attr('number', { defaultValue: 1}),
+  popStorage: attr('number', {defaultValue: 1}),
+  pendingPopStorage: attr('number', {defaultValue: 0}),
+  workerPopStorage: alias('pendingPopStorage'),
+  workerBreeder: attr('number', {defaultValue: 0}),
+
   food: attr('number', { defaultValue: 0 }),
+  foodStorage: attr('number', {defaultValue: 1}),
+  pendingFoodStorage: attr('number', {defaultValue: 0}),
+  workerFoodStorage: alias('pendingFoodStorage'),
+  workerHunter: attr('number', {defaultValue: 0}),
+
   material: attr('number', { defaultValue: 0 }),
+  materialStorage: attr('number', {defaultValue: 1}),
+  pendingMaterialStorage: attr('number', { defaultValue: 0}),
+  workerMaterialStorage: alias('pendingMaterialStorage'),
+  workerGatherer: attr('number', {defaultValue: 0}),
+
   spellPoints: attr('number', {defaultValue: 5}),
   maxSpellPoints: attr('number', {defaultValue: 5}),
-  workerBreeder: attr('number', {defaultValue: 0}),
-  workerHunter: attr('number', {defaultValue: 0}),
-  workerGatherer: attr('number', {defaultValue: 0}),
-  popStorage: attr('number', {defaultValue: 1}),
-  foodStorage: attr('number', {defaultValue: 1}),
-  materialStorage: attr('number', {defaultValue: 1}),
 
   maxPop: computed('popStorage', function() {
     return 100*this.popStorage
@@ -31,8 +42,8 @@ export default Model.extend({
     return 1000*this.materialStorage
   }),
 
-  availableWorkers: computed('population', 'workerBreeder', 'workerHunter', 'workerGatherer', function() {
-    return this.population - this.workerHunter - this.workerBreeder - this.workerGatherer
+  availableWorkers: computed('population', 'workerBreeder', 'workerHunter', 'workerGatherer', 'workerPopStorage', 'workerFoodStorage', 'workerMaterialStorage', function() {
+    return this.population - this.workerHunter - this.workerBreeder - this.workerGatherer - this.workerPopStorage - this.workerFoodStorage - this.workerMaterialStorage
   }),
 
   popProduction: computed('workerBreeder', function() {
@@ -61,6 +72,14 @@ export default Model.extend({
   }),
 
   async nextTurn() {
+    //Buildings
+    this.set('popStorage', this.popStorage + this.pendingPopStorage)
+    this.set('pendingPopStorage', 0)
+    this.set('foodStorage', this.foodStorage + this.pendingFoodStorage)
+    this.set('pendingFoodStorage', 0)
+    this.set('materialStorage', this.materialStorage + this.pendingMaterialStorage)
+    this.set('pendingMaterialStorage', 0)
+
     this.set('material', this.material + this.materialProduction)
     this.set('food', this.food + this.foodProduction)
     this.set('population', this.population+this.popProduction)
