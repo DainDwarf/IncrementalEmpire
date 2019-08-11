d5433610860795dbe275604b5647c5399ae61e54
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-11 16:43:41 +0200

    Added spell for pop material.

---
+++ app/controllers/empire/material.js
@@ -0,0 +1,26 @@
+import Controller from '@ember/controller';
+import { inject as controller } from '@ember/controller';
+import { computed } from '@ember/object';
+import { lt, or } from '@ember/object/computed';
+
+export default Controller.extend({
+  empireCtl: controller('empire'),
+  isGenMaterialOnCooldown: lt('model.spellPoints', 1),
+  isGenMaterialDisabled: or('isGenMaterialOnCooldown', 'model.dead', 'empireCtl.isMaxMaterial'),
+  isGenMaterialAvailable: computed('model.type', function() {
+    return this.model.type == "religious"
+  }),
+
+  actions: {
+    async genMaterial(event) {
+      event.preventDefault();
+      let incr = 1
+      if (this.game.getUpgrade('Click Power').isActive) {
+        incr = Math.max(1, Math.floor(Math.sqrt(this.game.universe.mana)))
+      }
+      this.model.set('material', Math.min(this.model.material + incr, this.model.materialStorage))
+      this.model.set('spellPoints', this.model.spellPoints - 1)
+      await this.model.save()
+    },
+  },
+});
