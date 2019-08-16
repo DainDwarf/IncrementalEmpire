7deed6ad00f6c3d56d4f3538d6f3aa38b14acc8e
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-16 17:50:22 +0200

    Corrected the component EmpireBuilding test.
    Yes, this is not quite a component, but it fills its purpose of model binding quite nicely

--- tests/integration/components/empire-building-test.js
+++ tests/integration/components/empire-building-test.js
@@ -2,25 +2,52 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import resetStorages from 'ember-local-storage/test-support/reset-storage';
 
 module('Integration | Component | empire-building', function(hooks) {
   setupRenderingTest(hooks);
 
+  hooks.beforeEach(function() {
+    if (window.localStorage) {
+      window.localStorage.clear();
+    }
+    if (window.sessionStorage) {
+      window.sessionStorage.clear();
+    }
+    resetStorages();
+  });
+
+  hooks.afterEach(function() {
+    if (window.localStorage) {
+      window.localStorage.clear();
+    }
+    if (window.sessionStorage) {
+      window.sessionStorage.clear();
+    }
+    resetStorages();
+  });
+
+
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    let game = this.owner.lookup('service:game');
+    await game.load()
+    let buildingFactory = this.owner.lookup('service:buildingFactory');
+    let building = await buildingFactory.generate('food-production-1', 'empire')
+    this.set('building', building)
 
-    await render(hbs`<EmpireBuilding />`);
+    await render(hbs`<EmpireBuilding @building={{this.building}}/>`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.ok(this.element);
 
     // Template block usage:
     await render(hbs`
-      <EmpireBuilding>
+      <EmpireBuilding @building={{this.building}}>
         template block text
       </EmpireBuilding>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(this.element);
   });
 });
