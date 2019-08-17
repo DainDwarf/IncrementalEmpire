c885fe6f2142650dfacde913a96c7c0780a9048e
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-17 13:31:36 +0200

    Added a debug button for toggling upgrades.
    That's useful!

--- app/controllers/upgrades.js
+++ app/controllers/upgrades.js
@@ -1,9 +1,19 @@
 import Controller from '@ember/controller';
+import config from 'incremental-empire/config/environment';
 
 export default Controller.extend({
+  debug: config.APP.DEBUG,
+
   actions: {
     async buy(upgrade) {
       await this.game.buyUpgrade(upgrade)
+    },
+    async debugToggle(upgrade) {
+      if (this.debug) {
+        upgrade.toggleProperty('isActive')
+        await upgrade.save()
+        await this.game.buyUpgrade(upgrade)
+      }
     },
   },
 });
