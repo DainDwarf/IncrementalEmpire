455375ebb724097510f73c4cba683242227a0531
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-18 18:47:53 +0200

    Removed obsolete utils files.

--- app/utils/upgrade.js
+++
@@ -1,7 +0,0 @@
-import { computed } from '@ember/object';
-
-export default function upgrade(upgradeKey) {
-  return computed('game.upgrades.@each.isActive', function() {
-    return this.game.getUpgrade(upgradeKey).isActive
-  })
-}
