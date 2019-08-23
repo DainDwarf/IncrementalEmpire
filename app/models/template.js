4856e695ad1ddc529dc54eda28a25ef40b9cf6ef
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-08-23 22:38:08 +0200

    Rmoved some dead code.

--- app/models/template.js
+++ app/models/template.js
@@ -1,7 +1,5 @@
 import DS from 'ember-data';
 const { Model, attr } = DS;
-import { computed } from '@ember/object';
-import { alias } from '@ember/object/computed';
 
 export default Model.extend({
   // Save only TP attribution. Controller will be in charge to compute what it results into.
@@ -11,30 +9,4 @@ export default Model.extend({
   spellTP: attr('number', { defaultValue: 0}),
 
   empire: undefined,    // Set by game service on game load, and templates controller on template creation.
-
-  capitalPopulation: computed('empire.buildings.@each.{qty,code}', function() {
-    for (let b of this.empire.buildings) {
-      if (b.code.startsWith('capital-population-') && b.qty > 0) {
-        return b
-      }
-    }
-  }),
-
-  capitalFood: computed('empire.buildings.@each.{qty,code}', function() {
-    for (let b of this.empire.buildings) {
-      if (b.code.startsWith('capital-food-') && b.qty > 0) {
-        return b
-      }
-    }
-  }),
-
-  capitalMaterial: computed('empire.buildings.@each.{qty,code}', function() {
-    for (let b of this.empire.buildings) {
-      if (b.code.startsWith('capital-material-') && b.qty > 0) {
-        return b
-      }
-    }
-  }),
-
-  capitalName: alias('capitalPopulation.name'),
 });
