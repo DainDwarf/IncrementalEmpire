48623bfce3705048369f4de72da7d3f4dda7e8f9
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-09-22 18:18:19 +0200

    Added badges for available upgrade types.

--- app/controllers/upgrades.js
+++ app/controllers/upgrades.js
@@ -1,5 +1,12 @@
 import Controller from '@ember/controller';
+import { filterBy } from '@ember/object/computed';
 
 export default Controller.extend({
   tabRoute: 'upgrades.religious',
+  buyableUpgrades: filterBy('game.upgrades', 'canBuy'),
+  buyableReligious: filterBy('buyableUpgrades', 'type', 'religious'),
+  buyableEconomical: filterBy('buyableUpgrades', 'type', 'economical'),
+  buyableMilitary: filterBy('buyableUpgrades', 'type', 'military'),
+  buyableScientific: filterBy('buyableUpgrades', 'type', 'scientific'),
+  buyableOther: filterBy('buyableUpgrades', 'type', 'other'),
 });
